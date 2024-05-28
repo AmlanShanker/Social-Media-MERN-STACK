ffae3b38a48bf996f66eb250a07ca939c5a5fd44
Amlan Shanker <amlanshanker2005@gmail.com>
2024-05-28 12:20:06 +0530

    Did some modifications

--- server/index.js
+++ server/index.js
@@ -60,10 +60,10 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
 
     /* ADD DATA ONE TIME */
     // User.insertMany(users);
-    //Post.insertMany(posts);
+    // Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
